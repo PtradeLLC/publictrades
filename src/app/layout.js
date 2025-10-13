e96b07b15fcdaf3d54cd53bd8653232343c02afe
PubTrades <chrisb@publictrades.com>
2025-10-13 14:32:02 -0400

    updated node version

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,57 +1,31 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
-import Navbar from "../components/Navbar";
-// import { useAuth } from '../context/authContext';
-// import appwriteService from '../appwrite/config';
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { Theme } from '@radix-ui/themes';
+import Navbar from '@/components/Navbar';
+import AuthProvider from '@/context/authContext';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ 
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+ })
 
 export const metadata = {
-  title: "PublicTrades",
-  description: "Developing overlooked ideas into useable products",
-  // It's generally better to manage icons and manifest through the metadata object
-  // if you're using the App Router, but adding them directly to head works too.
-  // icons: {
-  //   icon: [
-  //     { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-  //     { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-  //   ],
-  //   apple: '/apple-touch-icon.png',
-  // },
-  // manifest: '/site.webmanifest',
-};
+  title: 'Public-Trades',
+  description: 'A market-place for your digital products',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </head>
-      <body className={inter.className}>
+      <body className={`${inter.variable} font-sans`}>
+        <AuthProvider>
         <Theme>
-          <Navbar />
-          {children}
+        <Navbar />
+        {children}
         </Theme>
-      </body>
+        </AuthProvider>
+        </body>
     </html>
-  );
+  )
 }
